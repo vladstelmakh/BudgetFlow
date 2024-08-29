86e96e6de150da96a954a1c6da5886e31043cf8f
Vladyslav <129744372+vladstelmakh@users.noreply.github.com>
2024-08-29 15:38:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
 class Config:
-    MONGO_URI = 'mongodb+srv://vladysav:9QiWrHL59YrqkoVp@cluster0.3mmp8xg.mongodb.net/snt-electronics?retryWrites=true&w=majority'
+    MONGO_URI = 'your URI'
